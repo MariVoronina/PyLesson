8dc0052b85211315584c50abe9f2d21e473a137c
MariVoronina <99397546+MariVoronina@users.noreply.github.com>
2022-02-24 18:19:08 +0300

    Create Exercise_4.py

---
+++ HW_3/Exercise_4.py
@@ -0,0 +1,38 @@
+import random
+
+def password():
+    l = random.randint(7,10)
+    lst = []
+    lst_1 = []
+    for i in range(33,127):
+        lst.append(chr(i))
+    for i in range(l):
+        lst_1.append(random.choice(lst))
+    return ''.join(lst_1)
+
+def protect_password(s: str):
+    if len(s) < 8:
+        return False
+    else:
+        count_int = 0
+        count_title = 0
+        count_lower = 0
+        for i in s:
+            if i in '123456789':
+                count_int +=1
+            elif i == i.title():
+                count_title +=1
+            else:
+                count_lower +=1
+        if count_title >= 1 and count_lower >= 1 and count_int >= 1:
+            return True
+        else:
+            return False
+
+kol_pop = 1
+pas = password()
+while not(protect_password(pas)):
+    kol_pop += 1
+    pas = password()
+print("Количество попыток: ", kol_pop)
+print("Правильный пароль: ",pas)
