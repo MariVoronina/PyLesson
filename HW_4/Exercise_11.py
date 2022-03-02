a2f31c473a8934918c3fda1c66745bbc0e472628
MariVoronina <99397546+MariVoronina@users.noreply.github.com>
2022-03-02 20:48:06 +0300

    Create Exercise_11.py

---
+++ HW_4/Exercise_11.py
@@ -0,0 +1,9 @@
+text = '''Call me Ishmael. Some years ago - never mind how long precisely - having
+little or no money in my purse, and nothing particular to interest me
+on shore, I thought I would sail about a little and see the watery part
+of the world. It is a way I have of driving off the spleen, and regulating
+the circulation. - Moby Dick'''
+lst = text.split("\n")
+lst1 = [i.split() for i in lst]
+lst2 = [[item[i] if len(item[i]) > 3 else "0" for i in range(len(item))] for item in lst1]
+print([[item[i] for i in range(len(item)) if item[i] != "0"] for item in lst2])
